6390d437c961339efce5f0fc0aacdbaf5107ec36
mirandaxgui <114193951+mirandaxgui@users.noreply.github.com>
2024-08-12 15:53:44 -0300

    Adding the backend to the repository.

---
+++ planner/src/main/java/com/rocketseat/planner/activity/ActivityService.java
@@ -0,0 +1,36 @@
+package com.rocketseat.planner.activity;
+
+import com.rocketseat.planner.trip.Trip;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class ActivityService {
+
+    @Autowired
+    private ActivityRepository repository;
+
+    public ActivityResponse registerActivity(ActivityRequestPayload payload, Trip trip){
+        Activity newActivity = new Activity(payload.title(), payload.occurs_at(), trip);
+
+        this.repository.save(newActivity);
+
+        return new ActivityResponse(newActivity.getId());
+    }
+
+    public List<ActivityData> getAllActivitiesFromId(UUID tripId){
+        return this.repository.findByTripId(tripId).stream().map(activity -> new ActivityData(activity.getId(), activity.getTitle(), activity.getOccursAt())).toList();
+    }
+    
+    public void deleteActivity(Activity activity){
+        repository.delete(activity);
+    }
+
+    public Optional<Activity> findById(UUID activityId) {
+        return repository.findById(activityId);
+    }
+}
---
+++ planner/src/main/java/com/rocketseat/planner/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.rocketseat.planner.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173") // Altere para a URL do seu front-end
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}

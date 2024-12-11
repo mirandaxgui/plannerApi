0b4c4f96b0b75cb0e5ade9411c7558e57058b503
Guilherme Miranda <mirandax.gui13@gmail.com>
2024-12-10 22:37:45 -0300

    Adding HTTP requisition to CORS Config

--- src/main/java/com/rocketseat/planner/config/CorsConfig.java
+++ src/main/java/com/rocketseat/planner/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://mirandaxgui.github.io")
+                .allowedOrigins("http://mirandaxgui.github.io", "https://mirandaxgui.github.io")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);

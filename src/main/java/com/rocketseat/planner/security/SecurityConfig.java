49cf9d0df53cd7cf337cef44f4d5ab55ddd49f80
Guilherme Miranda <mirandax.gui13@gmail.com>
2024-12-16 14:55:29 -0300

    Implementing CORS on SecFilter

--- src/main/java/com/rocketseat/planner/security/SecurityConfig.java
+++ src/main/java/com/rocketseat/planner/security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.rocketseat.planner.security;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,24 +13,34 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationFi
 
 @Configuration
 public class SecurityConfig {
-  @Autowired
-  private SecurityParticipantFilter securityParticipantFilter;
 
-  @Bean
-  SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-    http.csrf(csrf -> csrf.disable())
-      .authorizeHttpRequests(auth -> {
-        auth.requestMatchers("/participant/auth").permitAll()
-        .requestMatchers("/participant/register").permitAll();
-        auth.anyRequest().authenticated();
-      })
-      .addFilterBefore(securityParticipantFilter, BasicAuthenticationFilter.class)
-    ;
-    return http.build();
-  }
+    @Autowired
+    private SecurityParticipantFilter securityParticipantFilter;
 
-  @Bean
-  public PasswordEncoder passwordEncoder() {
-    return new BCryptPasswordEncoder();
-  }
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(cors -> cors.configurationSource(request -> {
+            var corsConfig = new org.springframework.web.cors.CorsConfiguration();
+            corsConfig.setAllowedOrigins(List.of("https://planner-front-gold.vercel.app"));
+            corsConfig.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+            corsConfig.setAllowedHeaders(List.of("*"));
+            corsConfig.setAllowCredentials(true);
+            return corsConfig;
+        }))
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/participant/auth").permitAll()
+                            .requestMatchers("/participant/register").permitAll();
+                    auth.anyRequest().authenticated();
+                })
+                .addFilterBefore(securityParticipantFilter, BasicAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }

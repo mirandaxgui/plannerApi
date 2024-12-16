a3107723d382e65178cfa0fb5a5b4446acdbe06e
Guilherme Miranda <mirandax.gui13@gmail.com>
2024-12-16 14:20:15 -0300

    Permitting Vercel

--- src/main/java/com/rocketseat/planner/config/CorsConfig.java
+++ src/main/java/com/rocketseat/planner/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://mirandaxgui.github.io", "http://localhost:4000","https://mirandaxgui.github.io")
+                .allowedOrigins("https://planner-front-gold.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
